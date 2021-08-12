7f45e978d89a22e1ba4053759386c30b88cb1ea9
Mostafa Abbaszadeh <45285488+mabbaszade@users.noreply.github.com>
2021-08-12 18:03:29 +0430

    Add files via upload

---
+++ inputs/in3.py
@@ -0,0 +1,22 @@
+ITERATION_NUM = 100
+MAX_POPULATION = 500
+CROSSOVER_RATE = 1
+MUTATION_RATE = 1
+
+supplies = {
+    'S1': 7,
+    'S2': 9,
+    'S3': 18
+}
+
+demands = {
+    'D1': 5,
+    'D2': 8,
+    'D3': 7,
+    'D4': 14
+}
+
+cost = [[19, 30, 50, 10],
+        [70, 30, 40, 60],
+        [40, 8, 70, 20]
+        ]
---
+++ inputs/in4.py
@@ -0,0 +1,21 @@
+ITERATION_NUM = 100
+MAX_POPULATION = 500
+CROSSOVER_RATE = 1
+MUTATION_RATE = 1
+
+supplies = {
+    'S1': 20,
+    'S2': 15,
+    'S3': 40
+}
+
+demands = {
+    'D1': 20,
+    'D2': 30,
+    'D3': 25
+}
+
+cost = [[2, 3, 1],
+        [5, 4, 8],
+        [5, 6, 8]
+        ]
